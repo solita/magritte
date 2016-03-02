07ba49919136c067b34941c8dc9c43ed97402cda
Timo Mihaljov <timo.mihaljov@solita.fi>
2016-03-02 17:37:08 +0200

    java: Mount the project directory into the CI container

--- java/docker/PROJECT-ci/create.sh
+++ java/docker/PROJECT-ci/create.sh
@@ -15,6 +15,7 @@ fi
 if ! container_exists "$name"; then
   echo "Creating container $name..."
   docker create --name $name \
+    -v "$(cd ../.. && pwd):/project" \
     -P \
     $name \
     >/dev/null
