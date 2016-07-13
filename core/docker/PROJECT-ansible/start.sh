75616e529b8910cb5b3736c0d29da314c66b8d74
Timo Mihaljov <timo.mihaljov@solita.fi>
2016-07-13 09:48:48 +0300

    Install roles on Ansible container startup
    This way the we can use the user's SSH keys and DNS settings to install roles
    from a private Git repository.

--- core/docker/PROJECT-ansible/start.sh
+++ core/docker/PROJECT-ansible/start.sh
@@ -10,6 +10,13 @@ if ! container_running "$name"; then
   ./create.sh
   echo "Starting container $name..."
   docker start "$name" >/dev/null
-  echo 'TODO: Wait until sshd responds'
+
+  # TODO: Replace with a proper polling loop
+  echo 'Waiting for sshd to start...'
+  sleep 5
+
   ../../imagination/update-inventories
+
+  echo 'Installing Ansible roles with ansible-galaxy...'
+   ../../.magritte/util/in-ansible-container ansible-galaxy install -v -r /etc/ansible/requirements.yml
 fi
